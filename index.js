ac951ed8684c34ff0ea6f94a0dda9973057122ab
mostafiz365 <rahmanmostafiz365@gmail.com>
2026-05-18 22:20:04 +0600

    create some post and get api with new database collection

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const dns = require("node:dns");
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const port = process.env.PORT || 5000;
@@ -23,6 +25,42 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    const db = client.db("ideavault")
+    const ideasCollection = db.collection("ideas");
+    const myIdeasCollection = db.collection("myIdeas");
+
+    app.get('/ideas', async(req, res) =>{
+      const result = await ideasCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/ideas/:id', async(req, res) =>{
+      const id = req.params.id;
+      const result = await ideasCollection.findOne({_id: new ObjectId(id)});
+      res.send(result)
+    })
+
+    app.post('/ideas', async(req, res) =>{
+      const newIdea = req.body;
+      const result = await ideasCollection.insertOne(newIdea);
+      res.send(result);
+    })
+
+    app.get('/myIdeas/:userId', async(req, res) =>{
+      const userId = req.params.userId;
+      const result = await myIdeasCollection.find({userId}).toArray();
+      res.send(result);
+    })
+
+    app.post('/myIdeas', async(req, res) =>{
+      const myIdeasData = req.body;
+      const result = await myIdeasCollection.insertOne(myIdeasData);
+      res.send(result)
+    })
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
